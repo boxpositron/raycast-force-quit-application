5d89259f51d7580afdd7c3e05511c1fc26a20c57
David Ibia <david.ibia@boxmarshall.com>
2023-09-10 16:35:38 +0100

    refactor(dump-applications.ts): simplify the script to retrieve application information by using the POSIX path of the application file instead of using shell commands and Finder API

--- src/scripts/dump-applications.ts
+++ src/scripts/dump-applications.ts
@@ -1,32 +1,22 @@
-export default `tell application "System Events"
-    set appInfo to {}
-    set appProcesses to every process whose background only is false
-    repeat with anApp in appProcesses
-        set appName to name of anApp
-        set appPID to unix id of anApp
-        
-        try
-            set appPath to do shell script "ps aux | awk '/" & appPID & "/ && !/awk/ {print $11}'"
-        on error errMsg
-            set appPath to ""
-            display dialog "Error retrieving path for " & appName & ": " & errMsg
-        end try
+export default `
+tell application "System Events"
+	set appInfo to {}
+	set appProcesses to every process whose background only is false
 
-        set lengthOfPath to count characters of appPath
+	repeat with anApp in appProcesses
+		set appName to name of anApp
+		set appPID to unix id of anApp
+		
+		try
+			set appPath to POSIX path of (application file of anApp as alias)
 
-        if lengthOfPath is 0 then
-            set end of appInfo to {appName, appPID, appPath, ""}
-        else
-            tell application "Finder"
-                try
-                    set iconPath to (get icon of file appPath as alias) as text
-                    set end of appInfo to {appName, appPID,appPath, iconPath}
-                on error
-                    set end of appInfo to {appName, appPID, appPath, ""}
-                end try
-            end tell
-        end if
-    end repeat
+		on error errMsg
+			set appPath to ""
+		end try
+		
+		set end of appInfo to {appName, appPID, appPath}
+	end repeat
 end tell
 return appInfo
+
 `;
